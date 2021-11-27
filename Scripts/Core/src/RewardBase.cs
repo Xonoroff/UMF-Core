76e2fa7308f8d354a1a4a53c17ade0d900a84408
Pavlo Prokopov <xonor@outlook.com>
2021-11-27 13:33:56 +0200

    Add serialization logic for rewards

--- Scripts/Core/src/RewardBase.cs
+++ Scripts/Core/src/RewardBase.cs
@@ -1,13 +1,21 @@
-﻿namespace MF.Core.Scripts.Core.src
+﻿using System;
+using UnityEngine;
+
+namespace MF.Core.Scripts.Core.src
 {
+    [Serializable]
     public class RewardBase : IReward
     {
+        [field: SerializeField]
         public string RewardId { get; set; }
-        
+
+        [field: SerializeField]
         public string RewardCategory { get; set; }
         
+        [field: SerializeField]
         public ulong Amount { get; set; }
         
+        [field: SerializeField]
         public ulong ExpiredAt { get; set; }
     }
 }
